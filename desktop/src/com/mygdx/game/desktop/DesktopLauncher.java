a4f140d74a5bc83291e2526ff1bbafedf343d23a
Kasia <katarzyna.tramea@gmail.com>
2021-10-06 15:47:42 +0200

    change app window settings in DesktopLauncher.java

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -6,7 +6,16 @@ import com.mygdx.game.StrawberrySnakeGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.resizable = false;
+		//window size
+		config.width = 420;  //660?
+		config.height = 225;
+
+		config.title = "Strawberry Snake";
+
 		new LwjglApplication(new StrawberrySnakeGame(), config);
 	}
 }
