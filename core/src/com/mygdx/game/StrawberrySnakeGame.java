42d025bb8fe016155b9c4ece5a65d8df40e21390
Kasia <katarzyna.tramea@gmail.com>
2021-10-06 15:40:31 +0200

    change app window -add white screen

--- core/src/com/mygdx/game/StrawberrySnakeGame.java
+++ core/src/com/mygdx/game/StrawberrySnakeGame.java
@@ -1,33 +1,30 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+
 
 public class StrawberrySnakeGame extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("strawberryPic.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(Color.SALMON);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 150,100);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
