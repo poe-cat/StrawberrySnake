af3ceadfb2a912ea854a10ebbf79758a1fab5da0
Kasia <katarzyna.tramea@gmail.com>
2021-10-06 15:56:40 +0200

    add constructor and draw() method to Snake.java

--- core/src/com/mygdx/game/Snake.java
+++ core/src/com/mygdx/game/Snake.java
@@ -1,4 +1,34 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.GridPoint2;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+
+    private final Texture texture;
+    private final List<GridPoint2> snakeParts;
+
+    public Snake(Texture texture) {
+
+        this.texture = texture;
+
+        snakeParts = new ArrayList<>();
+
+        snakeParts.add(new GridPoint2(90, 30));
+        snakeParts.add(new GridPoint2(75, 30));
+        snakeParts.add(new GridPoint2(60, 30));
+        snakeParts.add(new GridPoint2(45, 30));
+        snakeParts.add(new GridPoint2(30, 30));
+    }
+
+    //drawing snake body (adding his "parts" to snakeParts ArrayList)
+    public void draw(Batch batch) {
+
+        for (GridPoint2 pos : snakeParts) {
+            batch.draw(texture, pos.x, pos.y);
+        }
+    }
 }
