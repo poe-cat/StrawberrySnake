3390f82dc1c4e133d805fea3fe35ba5f8372038e
Kasia <katarzyna.tramea@gmail.com>
2021-10-07 08:59:20 +0200

    update render() and dispose() with texture fill and unfill methods (in StrawberrySnakeGame.java)

--- core/src/com/mygdx/game/StrawberrySnakeGame.java
+++ core/src/com/mygdx/game/StrawberrySnakeGame.java
@@ -3,28 +3,37 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class StrawberrySnakeGame extends ApplicationAdapter {
 
 	private SpriteBatch batch;
+	private Texture snakeImg;
+	private Snake snake;
 
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
+		snakeImg = new Texture("snake.png");
+		snake = new Snake(snakeImg);
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		//drawing snake
+		snake.draw(batch);
 		batch.end();
 	}
 
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
+		//release texture (snake is moving on)
+		snakeImg.dispose();
 	}
 }
