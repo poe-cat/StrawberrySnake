f105ae6ea186e4d65d8bc76cc83cf3730fc4d808
Kasia <katarzyna.tramea@gmail.com>
2021-10-08 14:33:46 +0200

    update Strawberry.java with constructor and final fields

--- core/src/com/mygdx/game/Strawberry.java
+++ core/src/com/mygdx/game/Strawberry.java
@@ -1,4 +1,17 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.GridPoint2;
+
 public class Strawberry {
+
+    private final GridPoint2 position;
+    private final Texture texture;
+
+    public Strawberry(Texture texture) {
+        this.texture = texture;
+        this.position = new GridPoint2();
+    }
+
+
 }
