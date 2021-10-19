e7edb28b5a203357a5450bb8050d9b690b87dda9
Kasia <katarzyna.tramea@gmail.com>
2021-10-19 09:35:49 +0200

    delete unused fields

--- core/src/com/mygdx/game/StrawberrySnakeGame.java
+++ core/src/com/mygdx/game/StrawberrySnakeGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -28,9 +29,6 @@ public class StrawberrySnakeGame extends ApplicationAdapter {
 	Sound soundNom;
 	Sound soundCrash;
 	Music music;
-
-	private String gameOverStr;
-	BitmapFont gameOverFont;
 
 
 	@Override
@@ -80,7 +78,7 @@ public class StrawberrySnakeGame extends ApplicationAdapter {
 		strawberry.draw(batch);
 
 		//display score
-		bitmapFont.setColor(118, 140, 0, 1);
+		bitmapFont.setColor(Color.YELLOW);
 		bitmapFont.draw(batch, yourScore, 400, 440);
 
 		batch.end();
