168feccac3c1f4abd7ade4a515bf401e422b24f3
Kasia <katarzyna.tramea@gmail.com>
2021-10-19 16:30:51 +0200

    create constructor with game reference

--- core/src/com/mygdx/game/TitleScreen.java
+++ core/src/com/mygdx/game/TitleScreen.java
@@ -3,4 +3,10 @@ package com.mygdx.game;
 import com.badlogic.gdx.ScreenAdapter;
 
 public class TitleScreen extends ScreenAdapter {
+
+    StrawberrySnakeGame game;
+
+    TitleScreen(StrawberrySnakeGame game) {
+        this.game = game;
+    }
 }
