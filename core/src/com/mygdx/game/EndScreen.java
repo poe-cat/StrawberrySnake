5fc7c7b9bdab99f229fbd9136b651776e210ec60
Kasia <katarzyna.tramea@gmail.com>
2021-10-20 12:29:05 +0200

    update show() and hide() in EndScreen.java

--- core/src/com/mygdx/game/EndScreen.java
+++ core/src/com/mygdx/game/EndScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 
@@ -14,7 +16,16 @@ public class EndScreen implements Screen {
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keyCode) {
 
+                if (keyCode == Input.Keys.Q) {
+                    game.setScreen(new TitleScreen(game));
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -39,7 +50,7 @@ public class EndScreen implements Screen {
 
     @Override
     public void hide() {
-
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
