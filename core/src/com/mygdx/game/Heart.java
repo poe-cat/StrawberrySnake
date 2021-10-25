4721e03d1f8c7397c58a56383e74fab725d7685f
Kasia <katarzyna.tramea@gmail.com>
2021-10-25 12:35:53 +0200

    update Heart.java with new concept

--- core/src/com/mygdx/game/Heart.java
+++ core/src/com/mygdx/game/Heart.java
@@ -2,17 +2,29 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.GridPoint2;
 
 public class Heart {
 
     private final Texture texture;
 
+    //hearts should be snake-like?
+    //private final TextureRegion heartTexture;
+
     public Heart(Texture texture) {
         this.texture = texture;
+
+
     }
 
     public void draw(Batch batch) {
         batch.draw(texture, 800, 410);
+    }
+
+    public void extendHearts() {
+
+        //heartParts.add(new GridPoint2(heartParts.get(heartParts.size() - 1)));
     }
 
 
