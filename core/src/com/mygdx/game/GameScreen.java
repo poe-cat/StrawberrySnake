40fd098f023d20bff9be33abce04d57887916e1e
Kasia <katarzyna.tramea@gmail.com>
2021-10-27 13:30:28 +0200

    relocate create() content to GameScreen.java

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -4,10 +4,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
 
     StrawberrySnakeGame game;
+
+    public static Texture backgroundTexture;
+    public static Sprite backgroundSprite;
+
+    SpriteBatch batch;
+    private Texture snakeImg;
+    private Texture strawImg;
+    private Texture heartImg, emptyheartImg;
+    private Texture gameOverImg, gameOverImg2;
+
+    private Snake snake;
+    private Strawberry strawberry;
+    private Heart heart;
+    private GameOver gOver;
+
+    private boolean gameOver;
+
+    private int score;
+    private String yourScore;
+    BitmapFont bitmapFont;
+
+    Sound soundNom;
+    Sound soundCrash;
+    Music music;
+
+    boolean paused;
 
     public GameScreen(StrawberrySnakeGame game) {
         this.game = game;
@@ -24,6 +56,37 @@ public class GameScreen implements Screen {
                 return true;
             }
         });
+
+        score = 0;
+        yourScore = "score: 0";
+        bitmapFont = new BitmapFont();
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("core/assets/8bit_bg.mp3"));
+        soundNom = Gdx.audio.newSound(Gdx.files.internal("core/assets/eat_food.mp3"));
+        soundCrash = Gdx.audio.newSound(Gdx.files.internal("core/assets/crash.ogg"));
+
+        batch = new SpriteBatch();
+
+        //background image
+        backgroundTexture = new Texture("background.jpg");
+        backgroundSprite = new Sprite(backgroundTexture);
+
+        snakeImg = new Texture("pinkSnake.png");
+        strawImg = new Texture("straw2.png");
+
+        heartImg = new Texture("heart.png");
+        emptyheartImg = new Texture("emptyheart.png");
+
+        gameOverImg = new Texture("blank.png");
+        gameOverImg2 = new Texture("333.png");
+
+        snake = new Snake(snakeImg);
+        strawberry = new Strawberry(strawImg);
+        heart = new Heart(heartImg);
+
+        gOver = new GameOver(gameOverImg);
+
+        initNewGame();
     }
 
     @Override
